5be0b73059f0d1e366b394be98ffec1a1acfb308
filippova_t <tanya-fef@yandex.ru>
2022-12-11 22:57:39 +0800

    домашка 3.3

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -2,54 +2,44 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.*;
 
 @Service
 public class FacultyServiceImpl implements FacultyService{
-    private final Map<Long, Faculty> faculties;
-    private long lastId = 0;
 
 
-    public FacultyServiceImpl() {
-        this.faculties = new HashMap<>();
+    private final FacultyRepository facultyRepository;
+
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
     }
 
     @Override
     public Faculty createFaculty(Faculty faculty) {
-        faculty.setId(++lastId);
-        faculties.put(lastId, faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty findFaculty(long id) {
-        return faculties.get(id);
+        return facultyRepository.findById(id).get();
     }
 
     @Override
     public Faculty editFaculty(Faculty faculty) {
-        if (faculties.containsKey(faculty.getId())) {
-            faculties.put(faculty.getId(), faculty);
-            return faculty;
-        }
-        return null;
+        return facultyRepository.save(faculty);
     }
 
     @Override
-    public Faculty deleteFaculty (long id) {
-        return faculties.remove(id);
+    public void deleteFaculty (long id) {
+        facultyRepository.deleteById(id);
     }
 
     @Override
     public Collection<Faculty> findFacultyByColor(String color) {
-        ArrayList<Faculty> result = new ArrayList<>();
-        for (Faculty faculty : faculties.values()){
-            if (Objects.equals(faculty.getColor(), color)) {
-                //faculty.getAge() == age) {
-                result.add(faculty);
-            }
-        } return result;
+        return facultyRepository.findByColor(color);
     }
 
 }
