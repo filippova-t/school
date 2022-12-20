561adcee9fb5e54ef7218cb9b276c8a1bb48e2e3
filippova_t <tanya-fef@yandex.ru>
2022-12-21 01:44:46 +0800

    домашка 3.6 выполнена

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,6 +1,6 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -53,6 +53,10 @@ public class Student {
         return faculty;
     }
 
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
