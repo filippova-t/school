07243f258290ef4c8e9e72e0092e3ab2cb540dac
filippova_t <tanya-fef@yandex.ru>
2022-12-26 22:01:12 +0800

    домашка 4.3 выполнена

--- src/main/resources/liquibase/scripts/homework_four_three.sql
+++ src/main/resources/liquibase/scripts/homework_four_three.sql
@@ -1,7 +1,7 @@
- - liquibase formatted sql
+ -- liquibase formatted sql
 
- - changeSet filippova-t:1
+ -- changeSet filippova-t:1
 CREATE INDEX student_name_index ON student (name);
 
- - changeSet filippova-t:2
+ -- changeSet filippova-t:2
 CREATE INDEX faculty_name_and_color_index ON faculty (name, color);
